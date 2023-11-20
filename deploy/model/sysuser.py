a5f5d31b0c2aaf8168181ddc597bc5d795db71dd
mingliang.gao <gaoming971366@163.com>
2023-11-20 21:30:50 +0800

    修改注释

--- deploy/model/sysuser.py
+++ deploy/model/sysuser.py
@@ -3,7 +3,7 @@
 """
 ------------------------------------------------
 describe:
-    Python                   DB
+    model class              DB table
     ---------------------------------------
     SysUserModel             sysuser
 
