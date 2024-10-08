cf8f9e4cc2b36d04181874c5f420eefbd24756cd
mingliang.gao <gaoming971366@163.com>
2024-10-08 22:30:05 +0800

    优化utils>新增分类、优化path方法

--- deploy/utils/utils.py
+++ deploy/utils/utils.py
@@ -19,6 +19,8 @@ usage:
     from utils import xxxx
     xxxx(p1, p2, ...)
 
+    2024.10.08 update：新增分类、优化path方法
+
 design:
     具体方法详情请参考注释
 
@@ -34,32 +36,18 @@ Life is short, I use python.
 ------------------------------------------------
 """
 import os
-import sys
-import inspect
 import hashlib
 import time
 import subprocess
 import platform
 from datetime import datetime, timedelta
-from pathlib import Path as pathlib_path, PurePath as pathlib_purepath
+from pathlib import Path, PurePath
 # from functools import wraps
 from flask import session
 from deploy.config import ADMIN, ADMIN_AUTH_LIST
 
 
-def get_cur_folder():
-    """
-    get current folder, solve is or not frozen of the script
-
-    :return: the file current folder
-    """
-    if getattr(sys, "frozen", False):
-        return os.path.dirname(os.path.abspath(__file__))
-    else:
-        cur_folder = os.path.dirname(inspect.getfile(inspect.currentframe()))
-        return os.path.abspath(cur_folder)
-
-
+""" - - - - - - - - - - - - - - - - - 加密类 - - - - - - - - - - - - - - - - -"""
 def md5(v):
     """
     字符串md5加密
@@ -72,6 +60,27 @@ def md5(v):
     return hashlib.md5(v).hexdigest()
 
 
+def filename2md5(rtx_id: str = None, file_name: str = None, _type: str = 'file'):
+    """
+    get local store file name by md5 value
+
+    :param rtx_id: rtx id
+    :param file_name: file name
+    :param _type: file type, is file,image, and so on.
+    :return:
+    result is tuple
+    param1: md5 value no suffix
+    param2: md5 value have suffix
+    """
+    file_names = os.path.splitext(file_name)
+    suffix = (file_names[1]).lower() if len(file_names) > 1 else ''
+    _v = file_name + get_now() + _type + rtx_id if rtx_id \
+        else file_name + get_now() + _type
+    md5_v = md5(_v)
+    return md5_v, md5_v + suffix if suffix else md5_v
+
+
+""" - - - - - - - - - - - - - - - - - 时间、日期 - - - - - - - - - - - - - - - - -"""
 def s2d(s, fmt="%Y-%m-%d %H:%M:%S"):
     """
     字符串转日期
@@ -104,14 +113,15 @@ def d2ts(d):
     return time.mktime(d.timetuple())
 
 
-def s2ts(s, format="%Y-%m-%d %H:%M:%S"):
+def s2ts(s, fmt="%Y-%m-%d %H:%M:%S"):
     """
     字符串转ts
 
     :param s: sting type parameter
+    :param fmt: transfer to formatter
     :return: time.time type
     """
-    d = s2d(s, format)
+    d = s2d(s, fmt)
     return d2ts(d)
 
 
@@ -134,7 +144,7 @@ def dura_date(d1, d2, need_d=False):
         seconds = d.seconds
         mins = seconds / 60.00
         hours = mins / 60.00
-        return seconds, mins, hours
+        return hours, mins, seconds
     return d
 
 
@@ -156,13 +166,13 @@ def get_now_date():
     return datetime.now().date()
 
 
-def get_now(format="%Y-%m-%d %H:%M:%S"):
+def get_now(fmt="%Y-%m-%d %H:%M:%S"):
     """
     获取当前时间，字符串类型
 
     :return: to return the now of string type
     """
-    return d2s(datetime.now(), format)
+    return d2s(datetime.now(), fmt)
 
 
 def get_week_day(date):
@@ -172,88 +182,9 @@ def get_week_day(date):
     :param date: date
     :return: week
     """
-    weekdaylist = ('星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天')
-    weekday = weekdaylist[date.weekday()]
+    weekday_list = ('星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天')
+    weekday = weekday_list[date.weekday()]
     return weekday
-
-
-def get_user_id():
-    """
-    get current request link user rtx id
-    :return: rtx id or None
-    """
-    return session.get('rtx-id') or session.get('user_id')
-
-
-def get_real_ip(request):
-    """
-    get request real ip
-
-    :param request: flask api request object
-    """
-    if not request.headers.getlist("X-Forwarded-For"):
-        ip = request.remote_addr
-    else:
-        ip = request.headers.getlist("X-Forwarded-For")[0]
-    return ip
-
-
-def get_rtx_id(request):
-    """
-    get request user rtx-id
-
-    :param request: flask api request object
-    """
-    return request.headers.get("X-Rtx-Id") \
-        if request.headers.get("X-Rtx-Id") else ''
-
-
-def mk_dirs(path):
-    """
-    make folder（递归方式）
-
-    :param path: to make folder path
-    :return: path
-    """
-    os.makedirs(path)
-    return path
-
-
-def get_deploy_dir():
-    """
-    获取项目deploy目录
-
-    :return: abs deploy path
-    """
-    return os.path.dirname(get_cur_folder())
-
-
-def get_root_dir():
-    """
-    获取项目root目录
-
-    :return: project root directory
-    """
-    root = pathlib_path(__file__).resolve().parent.parent.parent
-    if root.is_absolute() and pathlib_path.exists(root):
-        return root
-    else:
-        return pathlib_path.cwd().resolve().parent.parent
-
-
-def v2decimal(x, y):
-    """
-    保留小数
-
-    :param x: value
-    :param y: point decimal
-    :return:
-    """
-    if not x:
-        return None
-    if x.find('.') > 0:
-        return round(x, y)
-    return int(x)
 
 
 def get_month_list():
@@ -264,39 +195,6 @@ def get_month_list():
     """
     return ['1月', '2月', '3月', '4月', '5月', '6月',
             '7月', '8月', '9月', '10月', '11月', '12月']
-
-
-def filename2md5(rtx_id: str = None, file_name: str = None, _type: str = 'file'):
-    """
-    get local store file name by md5 value
-
-    :param rtx_id: rtx id
-    :param file_name: file name
-    :param _type: file type, is file,image, and so on.
-    :return:
-    result is tuple
-    param1: md5 value no suffix
-    param2: md5 value have suffix
-    """
-    file_names = os.path.splitext(file_name)
-    suffix = (file_names[1]).lower() if len(file_names) > 1 else ''
-    _v = file_name + get_now() + _type + rtx_id if rtx_id \
-        else file_name + get_now() + _type
-    md5_v = md5(_v)
-    return md5_v, md5_v + suffix if suffix else md5_v
-
-
-def check_length(data, limit=10):
-    """
-    check data length
-
-    :param data: check data
-    :param limit: length limit, default value is 10
-    return True or False
-    """
-    if not data:
-        return True
-    return True if len(data) <= limit else False
 
 
 def get_day_week_date(query_date):
@@ -329,6 +227,121 @@ def get_day_week_date(query_date):
     return _res
 
 
+""" - - - - - - - - - - - - - - - - - 用户类 - - - - - - - - - - - - - - - - -"""
+def get_user_id():
+    """
+    get current request link user rtx id
+    :return: rtx id or None
+    """
+    return session.get('rtx-id') or session.get('user_id')
+
+
+def get_real_ip(request):
+    """
+    get request real ip
+
+    :param request: flask api request object
+    """
+    if not request.headers.getlist("X-Forwarded-For"):
+        ip = request.remote_addr
+    else:
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    return ip
+
+
+def get_rtx_id(request):
+    """
+    get request user rtx-id
+
+    :param request: flask api request object
+    """
+    return request.headers.get("X-Rtx-Id") \
+        if request.headers.get("X-Rtx-Id") else ''
+
+
+""" - - - - - - - - - - - - - - - - - 文件、目录 - - - - - - - - - - - - - - - - -"""
+def mk_dirs(path):
+    """
+    make folder（递归方式）
+
+    :param path: to make folder path
+    :return: path
+    """
+    os.makedirs(path)
+    return path
+
+
+def get_cur_folder():
+    """
+    get current folder, solve is or not frozen of the script
+
+    :return: the file current folder
+    """
+    # config file absolute path
+    current_abspath_file = Path(__file__).resolve()
+    if not current_abspath_file.is_absolute():
+        _os_path = os.path.dirname(os.path.abspath(__file__))
+        current_abspath_file = Path(_os_path)
+
+    return current_abspath_file.parent
+
+
+def get_deploy_folder():
+    """
+    获取项目deploy目录
+
+    :return: abs deploy path
+    """
+    if not get_cur_folder().is_absolute() \
+            or not get_cur_folder().exists():
+        return None
+
+    return get_cur_folder().parent
+
+
+def get_root_folder():
+    """
+    获取项目root目录
+
+    :return: project root directory
+    """
+    if not get_deploy_folder().is_absolute() \
+            or not get_deploy_folder().exists():
+        return None
+
+    return get_deploy_folder().parent
+
+
+""" - - - - - - - - - - - - - - - - - 参数校验类 - - - - - - - - - - - - - - - - -"""
+def v2decimal(x, y):
+    """
+    保留小数
+
+    :param x: value
+    :param y: point decimal
+    :return:
+    """
+    if not x:
+        return None
+    if x.find('.') > 0:
+        return round(x, y)
+    return int(x)
+
+
+def check_length(data, limit=10):
+    """
+    check data length
+
+    :param data: check data
+    :param limit: length limit, default value is 10
+    return True or False
+    """
+    if not data:
+        return True
+    return True if len(data) <= limit else False
+
+
+""" - - - - - - - - - - - - - - - - - 信息类、通讯类 - - - - - - - - - - - - - - - - -"""
 def ping(ip: str, **kwargs):
     """
     insect to ping the ip connection
@@ -413,6 +426,7 @@ def host_os():
     return os_code, _detail
 
 
+""" - - - - - - - - - - - - - - - - - 权限类 - - - - - - - - - - - - - - - - -"""
 def auth_rtx_join(rtx_list=None) -> list:
     """
     管理员特殊数据权限
@@ -454,6 +468,7 @@ def api_inspect_rtx() -> dict:
     pass
 
 
+""" - - - - - - - - - - - - - - - - - 数据类 - - - - - - - - - - - - - - - - -"""
 def get_file_size(path, unit: str = 'KB'):
     """
     获取传入的文件大小，以默认KB大小返回
